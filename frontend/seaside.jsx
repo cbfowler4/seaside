f3542f4b425215baf3b23601c8373eb962b408bb
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2017-12-28 16:24:10 -0500

    refactored to use App and root

--- frontend/seaside.jsx
+++ frontend/seaside.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener("DOMContentLoaded", ()=> {
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Seaside</h1>, root);
+  ReactDOM.render(<Root store={configureStore()}/>, root);
   // window.store = configureStore();
 });
