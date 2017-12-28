60626cf873d2f9d7a31462ca67aa6644fb43ffd7
Bryan Fowler <cbfowler4@gmail.com>
2017-12-28 17:22:19 -0500

    nav bar started

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import NavListContainer from './nav_list/nav_list_container';
 
 const App = () => (
   <div>
     <h1>Seaside</h1>
+    <NavListContainer />
   </div>
 );
 
