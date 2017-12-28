29b87dfab401f7b057ca65b9ca350defdb1e6842
Bryan Fowler <cbfowler4@gmail.com>
2017-12-28 16:16:23 -0500

    store, reducers complete

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,44 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+export const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const login = (user) => {
+  return (dispatch) => {
+    SessionAPIUtil.login(user).then((response) => {
+      dispatch(receiveCurrentUser(response));
+    },
+    (errors) => {
+      dispatch(receiveErrors(errors));
+    });
+  };
+};
+
+export const signup = (user) => {
+  return (dispatch) => {
+    SessionAPIUtil.signup(user).then((response) => {
+      dispatch(receiveCurrentUser(response));
+    });
+  };
+};
+
+export const logout = () => {
+  return (dispatch) => {
+    SessionAPIUtil.logout().then(() => {
+      dispatch(receiveCurrentUser(null));
+    },
+    (errors) => {
+      dispatch(receiveErrors(errors));
+    });
+  };
+};
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const defaultState = {currentUser: null};
+
+const sessionReducer = (state = defaultState, action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return action.user;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
