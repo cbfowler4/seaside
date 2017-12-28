096854eae4ee620c2a9240f8280bad384d194097
Bryan Fowler <cbfowler4@gmail.com>
2017-12-28 18:24:59 -0500

    login styling

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import NavListContainer from './nav_list/nav_list_container';
+import SessionFormContainer from './auth/session_form';
 
 const App = () => (
-  <div>
+  <div className="main">
     <h1>Seaside</h1>
     <NavListContainer />
+
+    <Route path="/login" component={SessionFormContainer}/>
   </div>
 );
 
