17b974678b4d3924db5fc2e55395a5ada8d6f7a2
Bryan Fowler <cbfowler4@gmail.com>
2018-01-03 13:24:41 -0500

    custom errors

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,6 +6,16 @@ class Api::UsersController < ApplicationController
       login(@user)
       render 'api/users/show'
     else
+      if @user.errors.keys.include?(:lname)
+        @user.errors.messages.delete(:lname)
+        @user.errors.messages[:Last] = ["name can't be blank"]
+      end
+
+      if @user.errors.keys.include?(:fname)
+        @user.errors.messages.delete(:fname)
+        @user.errors.messages[:First] = ["name can't be blank"]
+      end
+
       render json: @user.errors.full_messages, status: 422
     end
   end
