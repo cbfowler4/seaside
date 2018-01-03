462241fde78b169f6a0bf41496105118d9154148
Bryan Fowler <cbfowler4@gmail.com>
2018-01-03 16:55:10 -0500

    entities reducer added

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,10 @@ import { combineReducers } from 'redux';
 import errorsReducer from './errors_reducer';
 import sessionReducer from './session_reducer';
 import uiReducer from './ui_reducer';
-import roomReducer from './room_reducer';
+import entitiesReducer from './entities_reducer';
 
 const rootReducer = combineReducers({
-  rooms: roomReducer,
+  entities: entitiesReducer,
   session: sessionReducer,
   errors: errorsReducer,
   ui: uiReducer
