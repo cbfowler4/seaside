d1aac24b61448540a31628e6312d7e19caea69ba
Bryan Fowler <cbfowler4@gmail.com>
2018-01-03 15:39:52 -0500

    room table, controller, routes, validations and associations

---
+++ app/models/room.rb
@@ -0,0 +1,57 @@
+# == Schema Information
+#
+# Table name: rooms
+#
+#  id           :integer          not null, primary key
+#  host_id      :integer          not null
+#  title        :string           not null
+#  address      :string           not null
+#  lat          :float            not null
+#  lng          :float            not null
+#  price        :integer          not null
+#  max_guests   :integer          not null
+#  description  :text             not null
+#  beds         :integer          not null
+#  bathrooms    :integer          not null
+#  bedrooms     :integer          not null
+#  cancellation :string           not null
+#  min_stay     :integer          not null
+#  kitchen      :boolean          default(FALSE)
+#  wifi         :boolean          default(FALSE)
+#  washer       :boolean          default(FALSE)
+#  dryer        :boolean          default(FALSE)
+#  gym          :boolean          default(FALSE)
+#  hot_tub      :boolean          default(FALSE)
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Room < ApplicationRecord
+  validates :host_id,
+            :title,
+            :address,
+            :lat,
+            :lng,
+            :price,
+            :max_guests,
+            :description,
+            :beds,
+            :bathrooms,
+            :bedrooms,
+            :cancellation,
+            :min_stay,
+            :kitchen,
+            :wifi,
+            :washer,
+            :dryer,
+            :gym,
+            :hot_tub, presence: true
+
+  validates :host_id, uniqueness: true
+
+  belongs_to :host,
+    foreign_key: :host_id,
+    primary_key: :id,
+    class_name: :User
+
+end
