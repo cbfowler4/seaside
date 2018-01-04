f3508da31601ec48aa37fc0b37147e0b633a3eb5
Bryan Fowler <cbfowler4@gmail.com>
2018-01-04 11:04:25 -0500

    pre map refactor

--- frontend/actions/room_index_actions.js
+++ frontend/actions/room_index_actions.js
@@ -1,11 +1,19 @@
 import * as RoomsAPIUtil from '../util/rooms_api_util';
 
 export const RECEIVE_ROOMS = "RECEIVE_ROOMS";
+export const RECEIVE_BOUNDS = "RECEIVE_BOUNDS";
 
 export const receiveRooms = (rooms) => {
   return ({
     type: RECEIVE_ROOMS,
     rooms
+  });
+};
+
+export const receiveBounds = (bounds) => {
+  return ({
+    type: RECEIVE_BOUNDS,
+    bounds
   });
 };
 
