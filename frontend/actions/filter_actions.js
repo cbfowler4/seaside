395b1d7f3439908c523ec2edb61b58e37c3ac542
Bryan Fowler <cbfowler4@gmail.com>
2018-01-05 11:18:55 -0500

    guest button bugs mostly fixed

--- frontend/actions/filter_actions.js
+++ frontend/actions/filter_actions.js
@@ -1,5 +1,7 @@
 
 export const RECEIVE_GUESTS = 'RECEIVE_GUESTS';
+export const CLEAR_GUEST_FILTER = 'CLEAR_GUEST_FILTER';
+
 export const CLEAR_ALL_FILTERS = 'CLEAR_ALL_FILTERS';
 
 export const updateFilters = (filter) => {
@@ -8,9 +10,18 @@ export const updateFilters = (filter) => {
       type: RECEIVE_GUESTS,
       guests: filter.guests
     });
-  } else {
-    return ({
-      type: CLEAR_ALL_FILTERS
-    });
+  }
+};
+
+export const clearFilter = (filterType) => {
+  switch (filterType) {
+    case 'guest':
+      return ({
+        type: CLEAR_GUEST_FILTER
+      });
+    default:
+      return ({
+        type: CLEAR_ALL_FILTERS
+      });
   }
 };
