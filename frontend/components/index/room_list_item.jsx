2a256209ca25a543d676bfe0782f6dd8182b2e6c
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-07 12:21:12 -0500

    markers created, highlights object when marker is hovered over

--- frontend/components/index/room_list_item.jsx
+++ frontend/components/index/room_list_item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const RoomListItem = ({room}) => {
   const {roomType, title, price} = room;
   return(
-    <div className='room-list-item'>
+    <div className={`room-list-item room-item-${room.id}`}>
       <div className='room-thumbnail'></div>
       <h3>{roomType}</h3>
       <h1>{title}</h1>
