18c3af287c4dd0acc1cb8846a3e4d67b50251ac3
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-09 13:50:26 -0500

    guest drop down styled

--- frontend/components/room_show/booking.jsx
+++ frontend/components/room_show/booking.jsx
@@ -7,10 +7,17 @@ import { DateRangePicker } from 'react-dates';
 class Booking extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {startDate: null, endDate: null, focusedInput: null, guests: {adult: 1, child: 0}};
+    this.state = {startDate: null, endDate: null, focusedInput: null, adult: 1, child: 0};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onDatesChange = this.onDatesChange.bind(this);
     this.onFocusChange = this.onFocusChange.bind(this);
+    this.getGuestSum = this.getGuestSum.bind(this);
+    this.toggleDropDown = this.toggleDropDown.bind(this);
+
+  }
+
+  stopProp(e) {
+    e.stopPropagation();
   }
 
   onDatesChange ({ startDate, endDate }) {
@@ -23,28 +30,95 @@ class Booking extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    dispatch(requestBooking(this.state));
+    this.props.requestBooking(this.state);
+  }
+
+  addGuest(guestType) {
+    return ((e) => {
+      let sumGuests = (this.state[guestType] + 1);
+      if (( this.getGuestSum()+ 1) <= this.props.room.max_guests) {
+        this.setState({[guestType]: sumGuests});
+      }
+    }).bind(this);
+  }
+
+  removeGuest(guestType) {
+    return ((e) => {
+      let sumGuests = (this.state[guestType] - 1);
+      if ((sumGuests > 0 ) || (sumGuests === 0 && guestType!=='adult')) {
+        this.setState({[guestType]: sumGuests});
+      }
+    }).bind(this);
+  }
+
+  toggleDropDown(e) {
+    e.stopPropagation();
+    const guestDD = document.getElementsByClassName('guest-dropdown');
+    if (guestDD[0].classList.contains('hidden-guest-dropdown')) {
+      guestDD[0].classList.remove('hidden-guest-dropdown');
+      guestDD[0].classList.add('active-guest-dropdown');
+    } else {
+      guestDD[0].classList.add('hidden-guest-dropdown');
+      guestDD[0].classList.remove('active-guest-dropdown');
+    }
+  }
+
+
+  guestDropDown() {
+    return (
+      <div className='guest-dropdown hidden-guest-dropdown' onClick={this.stopProp}>
+        <ul>
+          <li><div>Adults</div>
+            <img onClick={this.removeGuest('adult')} src={window.staticImages.minusImage}></img>
+            <span>{`${this.state.adult}+`}</span>
+            <img onClick={this.addGuest('adult')} src={window.staticImages.plusImage}></img>
+          </li>
+          <li><div>Children</div>
+            <img onClick={this.removeGuest('child')} src={window.staticImages.minusImage}></img>
+            <span>{`${this.state.child}+`}</span>
+            <img onClick={this.addGuest('child')} src={window.staticImages.plusImage}></img>
+          </li>
+          <div className='filter-links'>
+            <a onClick={this.toggleDropDown}>Close</a>
+          </div>
+        </ul>
+      </div>
+    );
+  }
+
+  getGuestSum() {
+    return (this.state.adult + this.state.child);
   }
 
   render() {
     return (
       <div className='booking-component'>
-        <h1><span>{`$${this.props.room.price}`}</span> per night</h1>
-        <h3>ratings here</h3>
+        <div className='header'>
+          <h1><span>{`$${this.props.room.price}`}</span> per night</h1>
+          <h3>ratings here</h3>
+        </div>
         <form onSubmit={this.handleSubmit}>
-          <div><DateRangePicker
-              startDate={this.state.startDate}
-              endDate={this.state.endDate}
-              onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
-              focusedInput={this.state.focusedInput}
-              onFocusChange={focusedInput => this.setState({ focusedInput })}
-              startDateId="dp_start_date"
-              endDateId="dp_end_date"
-              startDatePlaceholderText="Check In"
-              endDatePlaceholderText="Check Out"  />
-          </div>
-          <div>guest drop down</div>
-          <button>Request to Book</button>
+          <label>Dates
+            <div><DateRangePicker
+                startDate={this.state.startDate}
+                endDate={this.state.endDate}
+                onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })}
+                focusedInput={this.state.focusedInput}
+                onFocusChange={focusedInput => this.setState({ focusedInput })}
+                startDateId="dp_start_date"
+                endDateId="dp_end_date"
+                startDatePlaceholderText="Check In"
+                endDatePlaceholderText="Check Out"/>
+            </div>
+          </label>
+          <label>Guests
+            <div className='guest-input' onClick={this.toggleDropDown}>
+              {`${this.getGuestSum()} guest${this.getGuestSum() > 1 ? 's' : ''}`}
+              <img src={window.staticImages.arrow} />
+            </div>
+          </label>
+          {this.guestDropDown()}
+          <button className='booking-button'>Request to Book</button>
         </form>
 
       </div>
