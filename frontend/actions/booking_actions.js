8515fd8e2da9b28660289a6765daffb8902d5739
Bryan Fowler <cbfowler4@gmail.com>
2018-01-09 16:03:48 -0500

    booking works, but its shotty

--- frontend/actions/booking_actions.js
+++ frontend/actions/booking_actions.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 export const requestBooking = (booking) => {
   booking.startDate = booking.startDate.format('MM/DD/YYYY');
   booking.endDate = booking.endDate.format('MM/DD/YYYY');
+  booking.currentUser = booking.currentUser.id;
   return (dispatch) => {
     return BookingsAPIUtil.requestRoomBooking(booking).then((console.log('success')));
   };
