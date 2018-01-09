3fd740f24a710d66ed04b95f49ef03320124fd59
Bryan Fowler <cbfowler4@gmail.com>
2018-01-09 17:12:05 -0500

    post reviews table creation

--- db/migrate/20180109220850_create_reviews.rb
+++ db/migrate/20180109220850_create_reviews.rb
@@ -1,6 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
+      t.integer :booking_id, null: false
+      t.text :body, null: false
+      t.integer :rating, null: false
+      t.timestamps
     end
   end
 end
