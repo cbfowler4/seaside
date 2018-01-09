d2029e192e97f8859260c82e848254e1a4496e39
Bryan Fowler <cbfowler4@gmail.com>
2018-01-09 13:50:26 -0500

    guest drop down styled

---
+++ frontend/components/room_show/guest_dropdown.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+
+class GuestFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = this.props.guests;
+  }
+
+  addGuest(guestType) {
+    return (e) => {
+      let sumGuests = (this.state[guestType] + 1);
+      this.setState({[guestType]: sumGuests});
+    };
+  }
+
+  removeGuest(guestType) {
+    return ((e) => {
+      let sumGuests = (this.state[guestType] - 1);
+      if ((sumGuests > 0 ) || (sumGuests === 0 && guestType!=='adult')) {
+        this.setState({[guestType]: sumGuests});
+      }
+    }).bind(this);
+  }
+
+  updateFilters(guest) {
+    return ((e) => {
+      e.stopPropagation();
+      this.props.updateFilters({guests: guest});
+      this.props.handleClose();
+    }).bind(this);
+  }
+
+  clearFilter(e) {
+    e.stopPropagation();
+    this.props.clearFilter('guest');
+    this.props.handleClose();
+  }
+
+  stopProp(e) {
+    e.stopPropagation();
+  }
+
+
+  render() {
+    return (
+      <div className='filter-modal-main guest-modal' onClick={this.stopProp}>
+        <ul>
+          <li><div>Adults</div>
+            <img onClick={this.removeGuest('adult')} src={window.staticImages.minusImage}></img>
+            <span>{`${this.state.adult}+`}</span>
+            <img onClick={this.addGuest('adult')} src={window.staticImages.plusImage}></img>
+          </li>
+          <li><div>Children</div>
+            <img onClick={this.removeGuest('child')} src={window.staticImages.minusImage}></img>
+            <span>{`${this.state.child}+`}</span>
+            <img onClick={this.addGuest('child')} src={window.staticImages.plusImage}></img>
+          </li>
+          <div className='filter-links'>
+            <a onClick={this.clearFilter}>Clear Filter</a>
+            <a onClick={this.updateFilters(this.state)}>Apply</a>
+          </div>
+        </ul>
+      </div>
+    );
+  }
+}
+
+
+export default GuestFilter;
