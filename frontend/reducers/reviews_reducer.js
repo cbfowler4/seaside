63bcbe78ed0283eadf95d289bf4a584af4c9c3da
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-10 11:51:44 -0500

    pre-addition of author on review table

--- frontend/reducers/reviews_reducer.js
+++ frontend/reducers/reviews_reducer.js
@@ -4,7 +4,6 @@ import { merge } from 'lodash';
 const reviewsReducer = (state = {}, action) => {
   switch (action.type) {
     case RECEIVE_ROOM:
-      debugger
       return merge({}, state, action.reviews);
     default:
       return state;
