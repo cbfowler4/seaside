bd4863ed5ac4d467df7bee07797fab0021ebae0b
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-10 11:09:56 -0500

    reviews and seeds improved

---
+++ db/seeds/05_review_seeds.rb
@@ -0,0 +1,72 @@
+RENTER_REVIEWS_WITH_NAMES = [
+  'was a great person!',
+  ' please come back soon! ',
+  ' was the perfect guest! ',
+  ' could have been a better guest. ',
+  ' left the poop deck a mess! ',
+  ' caught a massive fish! ',
+  ' left early in the morning, very respectful. '
+]
+
+RENTER_REVIEWS_GENERAL = [
+  'Don\'t forget to watch out for pirates! ',
+  'You were one of our most respectful sailors. ',
+  'Not only did you stay on a ship, but you also built a ship. A friendship. ',
+  'Arr you left too soon! ',
+  'You were cool, went with the flow. ',
+  'Acted very fishy, but eventually warmed up. ',
+  'You "floated our boat" with your warm conversation. ',]
+
+RENTER_REVIEWS_SENDOFF = [
+  'We would love to have them again',
+  'We hope to see you again soon!',
+  'We have built a strong friendship',
+  'Anchors away! Thanks for staying!',
+  'Thank you for staying with us.',
+  'We hope to voyage the seven seas again soon.',
+  'When pirates strike, we want you aboard our vessel.']
+
+ROOM_REVIEWS_WITH_NAMES = [
+  ' thank you for letting us stay with you! ',
+  ' thank you so much! ',
+  ' was a great host and has a beautiful boat! ',
+  ' helped create a perfect weekend for me and my family! ',
+  ' owns an amazing boat and was an amazing host! ',
+  ' took us fishing on their boat and made us a fresh fish dinner! ',
+  ' thank you a million times over! ',
+  ' was not around during our stay, boat was clean. ',
+  ' has a huge personality! A lot of fun! ']
+
+ROOM_REVIEWS_SENDOFF = [
+  'We will be staying with you again soon!',
+  'We hope to see you again soon!',
+  'We have built a strong friendship',
+  'Anchors away! Thanks for hosting!',
+  'Thank you for allowing us to board your vessel.',
+  'We hope to voyage the seven seas again soon.',
+  'Watch out for pirates, sharks and large squid! LOL']
+
+
+
+class ReviewSeeds
+  def initialize(bookings)
+    @bookings = bookings
+    @reviews = []
+  end
+
+  def generateSeeds()
+    @bookings.each do |booking|
+      @reviews.push(Review.create!({
+        reviewable: booking.renter,
+        rating: (rand()*2+3).to_i,
+        body: "#{booking.renter.fname}#{RENTER_REVIEWS_WITH_NAMES.sample}#{RENTER_REVIEWS_GENERAL.sample}#{RENTER_REVIEWS_SENDOFF.sample}"
+        }))
+
+      @reviews.push(Review.create!({
+        reviewable: booking.room,
+        rating: (rand()*2+3).to_i,
+        body: "#{booking.room.host.fname}#{ROOM_REVIEWS_WITH_NAMES.sample}#{ROOM_REVIEWS_GENERAL.sample}#{ROOM_REVIEWS_SENDOFF.sample}"
+        }))
+    end
+  end
+end
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import roomReducer from './room_reducer';
 import photosReducer from './photos_reducer';
+import reviewsReducer from './reviews_reducer';
 
 const entitiesReducer = combineReducers({
   rooms: roomReducer,
-  photos: photosReducer
+  photos: photosReducer,
+  reviews: reviewReducer
 });
 
 export default entitiesReducer;
