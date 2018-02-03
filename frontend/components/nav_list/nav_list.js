852b9e742fe5cf225fe80af3e9f0f9fb8e3b2ed2
Bryan Fowler <cbfowler4@gmail.com>
2018-02-03 12:25:11 -0500

    added contact info, adjusted css on user show'

--- frontend/components/nav_list/nav_list.js
+++ frontend/components/nav_list/nav_list.js
@@ -26,12 +26,35 @@ class NavList extends React.Component {
   }
 
   render() {
+    const contactInfo = (
+      <div id="contact-info">
+        <ul>
+          <li><a
+            target='_blank'
+            id='website-link' href='http://www.bryanfowler.io'>
+            <i className="fa fa-home" aria-hidden="true"></i>
+          </a></li>
+          <li><a
+            target='_blank'
+            id='linkedin-link' href='https://www.linkedin.com/in/bryanfowlerme/'>
+            <i className="fa fa-linkedin" aria-hidden="true"></i>
+          </a></li>
+          <li><a
+            target='_blank'
+            id='github-link' href='https://github.com/cbfowler4/seaside'>
+            <i className="fa fa-github" aria-hidden="true"></i>
+          </a></li>
+        </ul>
+      </div>
+    );
+
     let userStatus;
     if (!this.props.currentUser) {
       userStatus = (
         <div className='nav-right'>
           <a onClick={this.handleClickAuth('signup')}>Sign Up</a>
           <a onClick={this.handleClickAuth('login')}>Log In</a>
+          {contactInfo}
         </div>
       );
     } else {
@@ -41,6 +64,7 @@ class NavList extends React.Component {
           <form onSubmit={this.handleLogout}>
             <button>Log Out</button>
           </form>
+          {contactInfo}
         </div>
       );
     }
